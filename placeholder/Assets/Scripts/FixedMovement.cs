9386080d3a90bc09103619953daad786734315c1
Raül Lerma <rlermaprg@gmail.com>
2023-02-03 21:21:19 +0100

    a

--- placeholder/Assets/Scripts/FixedMovement.cs
+++ placeholder/Assets/Scripts/FixedMovement.cs
@@ -5,16 +5,38 @@ using UnityEngine;
 public class FixedMovement : MonoBehaviour
 {
   private Transform tr_;
+  private Rigidbody rb_;
+  private Vector3 dir_;
+
   public float speed_;
+  public Transform startPos;
+  public Transform finalPos;
+
     // Start is called before the first frame update
     void Start()
     {
         tr_ = GetComponent<Transform>();
+        rb_ = GetComponent<Rigidbody>();
+
+        dir_ = new Vector3(speed_, 0.0f, 0.0f);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(tr_.position.x < startPos.position.x)
+        {
+          dir_ = new Vector3(-dir_.x, dir_.y, dir_.z);
+          tr_.position = new Vector3(startPos.position.x, tr_.position.y, tr_.position.z);
+        }
+
+        if (tr_.position.x > finalPos.position.x)
+        {
+          dir_ = new Vector3(-dir_.x, dir_.y, dir_.z);
+          tr_.position = new Vector3(finalPos.position.x, tr_.position.y, tr_.position.z);
+        }
+
+        tr_.position += dir_ * Time.deltaTime;
     }
+
 }
