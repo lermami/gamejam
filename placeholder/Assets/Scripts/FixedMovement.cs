66734869f942b66bf4be1d557495c6d2b7a5d186
Raül Lerma <rlermaprg@gmail.com>
2023-02-03 20:35:52 +0100

    a

---
+++ placeholder/Assets/Scripts/FixedMovement.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FixedMovement : MonoBehaviour
+{
+  private Transform tr_;
+  public float speed_;
+    // Start is called before the first frame update
+    void Start()
+    {
+        tr_ = GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
