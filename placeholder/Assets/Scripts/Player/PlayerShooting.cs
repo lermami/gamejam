f3513555cbaa35431827d9fcb9bd85142593a33a
Raül Lerma <rlermaprg@gmail.com>
2023-02-04 16:13:56 +0100

    a

--- placeholder/Assets/Scripts/Player/PlayerShooting.cs
+++ placeholder/Assets/Scripts/Player/PlayerShooting.cs
@@ -7,13 +7,15 @@ public class PlayerShooting : MonoBehaviour
   public float cadence;
   protected float fire_time;
   public GameObject bulletPrefab_;
+  public int nBullets;
 
     // Update is called once per frame
     void Update()
     {
-      if(Input.GetButton("Fire1") && fire_time <0){
+      if(Input.GetButton("Fire1") && fire_time < 0 && nBullets > 0){
           Instantiate(bulletPrefab_, new Vector3(transform.position.x, transform.position.y, 0.0f), Quaternion.identity);
           fire_time = cadence;
+          nBullets--;
       }
 
       if(fire_time >=  0){
