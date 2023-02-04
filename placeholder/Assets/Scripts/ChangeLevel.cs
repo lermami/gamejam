bd2aac28edfa080fd0d7c23dc850785572449cb7
fuertesco <fuertesco@esat-alumni.com>
2023-02-04 16:23:43 +0100

    1

--- placeholder/Assets/Scripts/ChangeLevel.cs
+++ placeholder/Assets/Scripts/ChangeLevel.cs
@@ -5,14 +5,21 @@ public class ChangeLevel : MonoBehaviour
 {
 
   public string text;
+  public bool need_input;
 
   // Start is called before the first frame update
   void Update()
   {
-    if (Input.GetKeyDown(KeyCode.Return))
+    if (Input.GetKeyDown(KeyCode.Return) && need_input)
     {
       SceneManager.LoadScene(text);
     }
 
+  }
+
+  void OnCollisionEnter(Collision other){
+    if (other.gameObject.CompareTag("Player")){
+      SceneManager.LoadScene(text);
+    }
   }
 }
