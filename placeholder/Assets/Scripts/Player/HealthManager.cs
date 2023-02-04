ab9527791b53b11ab3df8d3fed78055b9ac062b8
Raül Lerma <rlermaprg@gmail.com>
2023-02-04 15:27:27 +0100

    a

--- placeholder/Assets/Scripts/Player/HealthManager.cs
+++ placeholder/Assets/Scripts/Player/HealthManager.cs
@@ -6,25 +6,42 @@ public class HealthManager : MonoBehaviour
 {
   public int n_lifes;
   public bool isDead;
+  public float inmune_timer_;
+
+  private PlayerMovement playerMov_;
     // Start is called before the first frame update
     void Start()
     {
       isDead = false;
+      inmune_timer_ = 0;
+
+      if(GetComponent<PlayerMovement>() != null)
+      {
+        playerMov_ = GetComponent<PlayerMovement>();
+      }
     }
 
     // Update is called once per frame
     void Update()
     {
-
+      if(inmune_timer_ > 0)
+      {
+        inmune_timer_ -= Time.deltaTime;
+      }
     }
 
     public void RecieveDamage(int damage)
     {
-      n_lifes -= damage;
+      if(inmune_timer_ <= 0)
+      {
+        n_lifes -= damage;
+      }
       if (n_lifes <= 0)
       {
         Destroy(gameObject);
       }
+      inmune_timer_ = 0.5f;
+
     }
 
     public bool bIsDead()
