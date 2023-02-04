43e59f32a140742e0f92e1c09054429de4b3e36f
Raül Lerma <rlermaprg@gmail.com>
2023-02-04 11:53:24 +0100

    a

--- placeholder/Assets/Scripts/Enemies/TrapController.cs
+++ placeholder/Assets/Scripts/Enemies/TrapController.cs
@@ -7,12 +7,17 @@ public class TrapController : MonoBehaviour
   private bool triggered;
   private bool finished;
   private float aux_position;
+
+  private Vector3 check_box_pos;
+  private Vector3 check_box_size;
   // Start is called before the first frame update
   void Start()
   {
     triggered = false;
     finished = false;
-    aux_position = transform.position.y;
+    aux_position = transform.position.y + 1.0f;
+
+    check_box_size = new Vector3(0.5f, 0.5f, 1.0f);
   }
 
   // Update is called once per frame
@@ -20,18 +25,23 @@ public class TrapController : MonoBehaviour
   {
     if (triggered && !finished)
     {
-      transform.position += new Vector3(0.0f, 3.0f, 0.0f);
+      transform.position += new Vector3(0.0f, 1.0f, 0.0f);
 
       if(transform.position.y > aux_position)
       {
-        transform.position = new Vector3(transform.position.x, aux_position, 0.0f);
+        transform.position = new Vector3(transform.position.x, aux_position, 0.0f );
         finished = true;
       }
     }
   }
 
-  void OnTriggerEnter(Collider other) 
+  void FixedUpdate()
   {
-    if (!triggered) triggered = true;
+    if(!finished && Physics.CheckBox(new Vector3(transform.position.x, transform.position.y + 1.1f, 0.0f), check_box_size))
+    {
+      triggered = true;
+      Debug.Log("Triggered");
+    }
   }
+
 }
