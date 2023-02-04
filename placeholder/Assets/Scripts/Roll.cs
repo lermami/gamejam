9d9d69021058a1e1a78c83a53b0c37d215a2d043
fuertesco <fuertesco@esat-alumni.com>
2023-02-04 21:14:44 +0100

    1

---
+++ placeholder/Assets/Scripts/Roll.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Roll : MonoBehaviour
+{
+  public float roll_speed;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.eulerAngles += new Vector3(0, transform.rotation.y+roll_speed*Time.deltaTime, 0);
+    }
+}
