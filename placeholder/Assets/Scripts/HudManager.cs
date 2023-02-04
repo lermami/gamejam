b6df25a2286782435e9056839af1091ea901564f
Raül Lerma <rlermaprg@gmail.com>
2023-02-04 15:50:36 +0100

    a

---
+++ placeholder/Assets/Scripts/HudManager.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HudManager : MonoBehaviour
+{
+  public Image[] lifes = new Image[3];
+
+    public void ChangeLifes(int lifes)
+    {
+      
+    }
+}
