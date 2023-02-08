981cddf6f66aba677628b60c09bf22e55861f02a
lermami <lermami@esat-alumni.com>
2023-02-08 20:10:57 +0100

    Fix trigger

--- placeholder/Assets/Scripts/Player/PlayerColision.cs
+++ placeholder/Assets/Scripts/Player/PlayerColision.cs
@@ -5,27 +5,15 @@ using UnityEngine;
 public class PlayerColision : MonoBehaviour
 {
     private HealthManager healthM_;
-    private PlayerShooting player_shoot_;
+    public PlayerShooting player_shoot_;
 
     // Start is called before the first frame update
     void Start()
     {
         healthM_ = GetComponent<HealthManager>();
-        player_shoot_ = GetComponent<PlayerShooting>();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
-    //Movile platform floor
-    void OnCollisionEnter(Collision other){
-        if (other.gameObject.CompareTag("movile_platform") && Physics.CheckBox(transform.position + new Vector3(0 , -1.0f , 0), new Vector3(0.499f , 0.1f, 0.499f), transform.rotation)){
-            transform.parent = other.transform;
-        }
-
+    void OnTriggerEnter(Collider other){
         if (other.gameObject.CompareTag("Shield_PickUP")){
             Destroy(other.gameObject);
             healthM_.shielded_counter = 10;
@@ -33,8 +21,8 @@ public class PlayerColision : MonoBehaviour
 
         if (other.gameObject.CompareTag("Ammo_PickUP"))
         {
-            player_shoot_.AddAmmo(20);
             Destroy(other.gameObject);
+            player_shoot_.AddAmmo(20);
         }
 
         if (other.gameObject.CompareTag("Health_PickUP"))
@@ -43,6 +31,13 @@ public class PlayerColision : MonoBehaviour
             if(gameObject.GetComponent<HealthManager>().n_lifes < 3){
                 gameObject.GetComponent<HealthManager>().RecieveDamage(-1);
             }
+        }
+    }
+
+    //Movile platform floor
+    void OnCollisionEnter(Collision other){
+        if (other.gameObject.CompareTag("movile_platform") && Physics.CheckBox(transform.position + new Vector3(0 , -1.0f , 0), new Vector3(0.499f , 0.1f, 0.499f), transform.rotation)){
+            transform.parent = other.transform;
         }
     }
 
